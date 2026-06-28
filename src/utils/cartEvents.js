a68f268f5e202b4447cdef42a91c124f9c934684
Huynh Bao Long <huynhbaolong@gmail.com>
2026-06-28 17:54:36 +0700

    fix(cart): count distinct cart items

--- src/utils/cartEvents.js
+++ src/utils/cartEvents.js
@@ -1,7 +1,7 @@
 export const CART_UPDATED_EVENT = 'bookverse:cart-updated';
 
 export function getCartItemCount(cart) {
-  return (cart?.items || []).reduce((sum, item) => sum + (item.quantity || 0), 0);
+  return (cart?.items || []).length;
 }
 
 export function notifyCartUpdated(cart) {
