1fa98df3fd27040aad5ec8851641a8a37b8f8272
taiduc1001 <phantaiduc2005@gmail.com>
2026-07-18 09:46:46 +0000

    feat: for production

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['.vercel.app', '.linodeusercontent.com'],
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_PROXY_TARGET || 'http://localhost:8080',
         changeOrigin: true,
+      },
+      '/minio': {
+        target: 'http://host.docker.internal:9000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/minio/, ''),
       },
     },
   },
